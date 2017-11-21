1d0efa8e1392a88b0d2a1cf3164109da7056c7d0
Caoanlong <caoanlong@gmail.com>
2017-11-21 15:19:59 +0800

    gg

--- routes/api/shop.js
+++ routes/api/shop.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jwt-simple');
+var secret =require('../../config').secret;
 
 var GoldBeanType = require('../../models/GoldBeanType');
+var Member = require('../../models/Member');
+var AccountDetail = require('../../models/AccountDetail');
 
 //统一返回格式
 var responseData;
@@ -27,6 +31,23 @@ router.get('/goldBeanType', function(req, res) {
 			goldBeanTypeList: result
 		}
 		res.json(responseData)
+	})
+})
+/* 购买金豆 */
+router.post('/buyGoldBean', function(req, res) {
+	let token = (req.body && req.body.token) || (req.query && req.query.token) || req.headers['x-access-token']
+	let memberId = jwt.decode(token, secret.jwtTokenSecret).iss
+	let goldBeanNum = Number(req.body.goldBeanNum)
+	Member.findOne({_id: memberId}).exec(function(err, member) {
+		Member.update({_id: memberId},{
+			goldBean: member.goldBean + goldBeanNum
+		})
+		new AccountDetail({
+			member: memberId,
+			goldBeanChange: +goldBeanNum,
+			type: '充值',
+			info: ''
+		}).save()
 	})
 })
 
