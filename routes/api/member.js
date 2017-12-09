df0d22d8e7646fdae2404b7a4326e277206c5ddb
caoanlong <476977540@qq.com>
2017-12-09 08:30:54 +0800

    gggg

--- routes/api/member.js
+++ routes/api/member.js
@@ -6,6 +6,7 @@ const common = require('../../common')
 const Member = require('../../models/Member')
 const VerCode = require('../../models/VerCode')
 const AccountDetail = require('../../models/AccountDetail')
+const MemberPrize = require('../../models/MemberPrize')
 const secret = require('../../config').secret
 
 //统一返回格式
@@ -165,7 +166,7 @@ router.post('/login', (req, res, next) => {
 })
 
 /* 查看会员详情 */
-router.post('/info', function(req, res) {
+router.post('/info', (req, res) => {
 	let token = (req.body && req.body.token) || (req.query && req.query.token) || req.headers['x-access-token']
 	let memberId = jwt.decode(token,secret.jwtTokenSecret).iss
 	Member.findOne({_id: memberId}, [
@@ -175,7 +176,7 @@ router.post('/info', function(req, res) {
 			'goldBean',
 			'isDisabled',
 			'addTime'
-		], function(err, result) {
+		], (err, result) => {
 		if (err) {
 			responseData.code = 1
 			responseData.msg = '获取失败'
@@ -194,7 +195,7 @@ router.post('/info', function(req, res) {
 	})
 })
 /* 查看会员帐户明细 */
-router.get('/accountDetails',function(req, res) {
+router.get('/accountDetails', (req, res) => {
 	let token = (req.body && req.body.token) || (req.query && req.query.token) || req.headers['x-access-token']
 	let memberId = jwt.decode(token,secret.jwtTokenSecret).iss
 	let pageIndex = Number(req.query.pageIndex || 1)
@@ -208,7 +209,7 @@ router.get('/accountDetails',function(req, res) {
 		//取值不能小于1
 		pageIndex = Math.max(pageIndex, 1)
 		let skip = (pageIndex - 1) * pageSize
-		AccountDetail.find({member: memberId}).sort({_id: -1}).limit(pageSize).skip(skip).exec(function(err, accountDetail) {
+		AccountDetail.find({member: memberId}).sort({_id: -1}).limit(pageSize).skip(skip).exec((err, accountDetail) => {
 			if (err) {
 				responseData.code = 1
 				responseData.msg = '获取失败'
@@ -227,18 +228,84 @@ router.get('/accountDetails',function(req, res) {
 		})
 	})
 })
+/* 查看会员奖品列表 */
+router.get('/memberPrizeList', (req, res) => {
+	let token = (req.body && req.body.token) || (req.query && req.query.token) || req.headers['x-access-token']
+	let memberId = jwt.decode(token,secret.jwtTokenSecret).iss
+	let pageIndex = Number(req.query.pageIndex || 1)
+	let pageSize = 15
+	let pages = 0
+	MemberPrize.count({member: memberId}, (error, count) => {
+		//计算总页数
+		pages = Math.ceil(count / pageSize)
+		//取值不能超过pages
+		pageIndex = Math.min(pageIndex, pages)
+		//取值不能小于1
+		pageIndex = Math.max(pageIndex, 1)
+		let skip = (pageIndex - 1) * pageSize
+		MemberPrize.find({member: memberId}).populate('prize').sort({_id: -1}).limit(pageSize).skip(skip).exec((err, memberPrizelist) => {
+			if (err) {
+				responseData.code = 1
+				responseData.msg = '获取失败'
+				res.json(responseData)
+				return
+			}
+			responseData.msg = '获取成功'
+			responseData.data = {
+				memberPrizelist: memberPrizelist,
+				count: count,
+				pageSize: pageSize,
+				pageIndex: pageIndex,
+				pages: pages
+			}
+			res.json(responseData)
+		})
+	})
+})
+/* 查看会员奖品详情 */
+router.get('/memberPrizeDetail', (req, res) => {
+	let memberPrizeId = req.query.memberPrizeId
+	MemberPrize.findOne({_id: memberPrizeId}).populate('prize').exec((err, memberPrize) => {
+		if (err) {
+			responseData.code = 1
+			responseData.msg = '获取失败'
+			res.json(responseData)
+			return
+		}
+		responseData.msg = '获取成功'
+		responseData.data = memberPrize
+		res.json(responseData)
+	})
+})
+/* 添加会员奖品地址 */
+router.post('/AddMemberPrizeAddress', (req, res) => {
+	let memberPrizeId = req.query.memberPrizeId
+	let address = req.body.address
+	MemberPrize.update({_id: memberPrizeId}, {
+		address: address
+	}, err => {
+		if (err) {
+			responseData.code = 1
+			responseData.msg = '添加失败'
+			res.json(responseData)
+			return
+		}
+		responseData.msg = '添加成功'
+		res.json(responseData)
+	})
+})
 /* 上传头像 */
-router.post('/avatar',function(req, res) {
+router.post('/avatar', (req, res) => {
 	Member.update({_id: req.body.memberId}, {
 		avatar: req.body.avatar
-	}, function(error) {
+	}, error => {
 		if (error) {
 			responseData.code = 1
 			responseData.msg = '上传失败'
 			res.json(responseData)
 			return
 		}
-		Member.findOne({_id: req.body.memberId}).exec(function(err, member) {
+		Member.findOne({_id: req.body.memberId}).exec((err, member) => {
 			if (err) {
 				responseData.code = 2
 				responseData.msg = '上传失败'
